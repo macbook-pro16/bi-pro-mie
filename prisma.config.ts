996d49b593db2a8ca7e68eaaba3d28f3077a2afb
macbook-pro16 <yamasaki.sunauto@gmail.com>
2026-07-25 06:13:28 +0900

    update

--- prisma.config.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  url: process.env.DATABASE_URL || "postgresql://dummy:dummy@localhost:5432/dummy",
-  directUrl: process.env.DIRECT_URL || "postgresql://dummy:dummy@localhost:5432/dummy",
-};
